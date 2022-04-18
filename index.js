8c0d3844447b7803053469a87f8a82e09c542d4c
Amisha Kirti <amishakirti6410@gmail.com>
2022-04-18 12:24:03 +0530

    Added files

---
+++ index.js
@@ -0,0 +1,30 @@
+// Setting up the Variables
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
+
+
+//setting up the listener
+bars.addEventListener("click", barClicked, false);
+
+
+//setting up the clicked Effect
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
+
+// for heading not working
+$(document).ready(function(){
+    var mouseX, mouseY;
+    var ww = $( window ).width();
+    var wh = $( window ).height();
+    var traX, traY;
+    $(document).mousemove(function(e){
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      traX = ((4 * mouseX) / 570) + 40;
+      traY = ((4 * mouseY) / 570) + 50;
+      console.log(traX);
+      $(".title").css({"background-position": traX + "%" + traY + "%"});
+    });
+  });
